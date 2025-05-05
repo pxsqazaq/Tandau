cda9eeb7138740bc68c7090fc5506f80667d3b61
dimash <arystambekdimash005@gmail.com>
2025-05-05 22:46:00 +0500

    feat(pages callback): added new page to hanlde goole auth

---
+++ pages/auth/callback.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const userStore = useUserStore();
+const router = useRouter();
+
+onMounted(async () => {
+  await userStore.initUser();
+  if (userStore.isAuthenticated) {
+    router.replace("/");
+  } else {
+    router.replace("/sign-in");
+  }
+});
+</script>
+
+<template>
+  <div>Logging in...</div>
+</template>
